58a1498b1c85c624d8dfa7b4f3d751eb1facb21c
hebaran <36790650+hebaran@users.noreply.github.com>
2026-04-29 23:17:50 -0300

    feat: Criei todas as Rotas CRUD, sendo um Post para criar uma Pessoa no DB, um Get para listar todas as pessoas do DB, um Get por ID para retornar uma pessoa específica, um Put para alteração de nome da Pessoa pelo seu ID e um Delete para remover alguma Pessoa do DB através do seu ID.

--- PersonAPI/Routes/PersonRoute.cs
+++ PersonAPI/Routes/PersonRoute.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using PersonAPI.Data;
 using PersonAPI.Models;
 
 namespace PersonAPI.Routes;
@@ -6,6 +8,68 @@ public static class PersonRoute
 {
     public static void PersonRoutes(this WebApplication app)
     {
-        app.MapGet("/person", () => new PersonModel("Heitor"));
+        var personRoute = app.MapGroup("/person");
+
+        personRoute.MapPost("/",
+        async (PersonRequest request, PersonContext context) =>
+        {
+            var person = new PersonModel(request.Name);
+            await context.AddAsync(person);
+            await context.SaveChangesAsync();
+
+            return Results.Created();
+        });
+
+        personRoute.MapGet("/",
+        async (PersonContext context) =>
+        {
+            var people = await context.People.ToListAsync();
+            return Results.Ok(people);
+        });
+
+        personRoute.MapGet("/{id:guid}",
+        async (Guid id, PersonContext context) =>
+        {
+            var person = await context.People.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (person == null)
+            {
+                return Results.NotFound();
+            }
+
+            return Results.Ok(person);
+        });
+
+        personRoute.MapPut("/{id:guid}",
+        async (Guid id, PersonRequest request, PersonContext context) =>
+        {
+            var person = await context.People.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (person == null)
+            {
+                return Results.NotFound();
+            }
+
+            person.ChangeName(request.Name);
+            await context.SaveChangesAsync();
+
+            return Results.Ok(person);
+        });
+
+        personRoute.MapDelete("/{id:guid}",
+        async (Guid id, PersonContext context) =>
+        {
+            var person = await context.People.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (person == null)
+            {
+                return Results.NotFound();
+            }
+
+            context.People.Remove(person);
+            await context.SaveChangesAsync();
+
+            return Results.NoContent();
+        });
     }
 }
